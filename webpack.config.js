803bd974a96475568044cd561877263ada8d4d71
siddharth sharma <sandesh251189@gmail.com>
2018-05-03 01:59:38 -0400

    Adding project Starter

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry:[
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
